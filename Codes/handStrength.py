1fb3deb24df2d76c083ab628e0e8ad10e686f4a9
Tuhin Khare <tuhinkhare3@gmail.com>
2017-04-08 21:29:31 +0530

    Some modular changes

--- Codes/handStrength.py
+++ Codes/handStrength.py
@@ -13,46 +13,42 @@ def calc_strength(hand) :
         p *= prior_probabilities[i]
         # print p
         prob_x += p
-        # print "*** ",prob_x
+    print "*** ",prob_x
 
-    prob_x_given_wi = 0.0
+    prob_wi_given_x = 0.0
     for i in range (0,10) :
-        prob_x_given_wi = 1
+        prob_wi_given_x = 1
         for j in range (0,len(hand)) :
-            prob_x_given_wi *= likelihood[hand[j]][i]
-        prob_x_given_wi *= prior_probabilities[i]
-        prob_x_given_wi /= prob_x
+            prob_wi_given_x *= likelihood[hand[j]][i]
+        prob_wi_given_x *= prior_probabilities[i]
+        prob_wi_given_x /= prob_x
 
-        print prob_x_given_wi
+        #print prob_wi_given_x
 
-        strength.append(prob_x_given_wi)
+        strength.append(prob_wi_given_x)
 
     max_strength = max(strength)
     max_index = strength.index(max_strength)
 
-    print max_strength,max_index
+    #print max_strength,max_index
 
     u_x = 1/(prior_probabilities[max_index] * 1.0)
 
-    print u_x
+    #print u_x
 
     prob_p_for_lower_max_index = 0.0
 
     for i in range (0,max_index) :
-        prob_x_given_wi = 1
-        for j in range (0,len(hand)) :
-            prob_x_given_wi *= likelihood[hand[j]][i]
-        prob_x_given_wi *= prior_probabilities[i]
-        prob_x_given_wi /= prob_x
-        prob_x_given_wi *= 1/(prior_probabilities[i])
+        prob_wi_given_x = strength[i];
+        prob_wi_given_x *= (1/(prior_probabilities[i]))
 
-        prob_p_for_lower_max_index += prob_x_given_wi
+        prob_p_for_lower_max_index += prob_wi_given_x
 
-    print prob_p_for_lower_max_index
+    #print prob_p_for_lower_max_index
 
     s_x = u_x + prob_p_for_lower_max_index
 
     return s_x
 
-hand = ['DA','DK','DJ','DQ','D10','D10','S10']
+hand = ['DA','DK','DJ','DQ','D10','C10','HK']
 print "**",calc_strength(hand)
