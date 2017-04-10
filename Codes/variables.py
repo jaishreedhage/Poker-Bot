f717cdf9c8a1f30280bea3ccc5e02707dfeef2cc
Jaishree <jaishree.babu96@gmail.com>
2017-04-10 12:22:11 +0530

    Knowledge base with pandas added

--- Codes/variables.py
+++ Codes/variables.py
@@ -5,3 +5,12 @@ P1FOLD = 0
 P2FOLD = 0
 P1BETRAISE = 0
 P2BETRAISE = 0
+
+file_extn = ".csv"
+csv_flop = "flop_base"
+csv_turn = 'turn_base'
+csv_river = 'river_base'
+
+headers = ['Fold','Check','Call','Bet-1','Bet-2','Bet-5','Bet-7']
+
+index = [0,200,500,2000,10000]
